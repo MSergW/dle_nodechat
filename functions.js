6f95832c794b7a88266f27ee29ceee11a6d3d811
MSW <msw@mail.ru>
2012-09-26 11:49:46 +0300

    Initial commit

---
+++ functions.js
@@ -0,0 +1,106 @@
+/*
+=====================================================
+ Модуль: NodeChat for DLE
+ Версия: 1.0.3
+-----------------------------------------------------
+ Автор: MSW
+ Сайт:  http://0-web.ru/
+-----------------------------------------------------
+ Copyright (c) 2012 MSW
+=====================================================
+ Данный код защищен авторскими правами
+=====================================================
+ Файл: functions.js
+=====================================================
+*/
+// *** Парсинг куков *** //
+exports.parse_cookies = function( _cookies ) {
+	var cookies = {};
+	_cookies && _cookies.split('; ').forEach( function( cookie ) {
+		var parts = cookie.split('=');
+		cookies[ parts[0] ] = parts[1] || '';
+	});
+	return cookies;
+}
+
+// *** Парсинг PHP сессий *** //
+exports.parse_session = function( _sessions ) {
+	var sessions = {};
+	_sessions && _sessions.split(';').forEach( function( session ) {
+		var parts = session.split('|');
+		if( parts[0] == "dle_user_id" ) sessions["dle_user_id"] = parse_session_val( parts[1] );
+		else if( parts[0] == "dle_password" ) sessions["dle_password"] = parse_session_val( parts[1] );
+	});
+	return sessions;
+}
+function parse_session_val( val ) {
+	var partz = val.split(':');
+	if(partz[2]) return partz[2].replace(/"/g, "");
+	else return 0;
+}
+
+// *** Добавление нуля в дату и время *** //
+function addZero(i) {
+	return (i < 10) ? "0"+i : i;
+}
+
+// *** Парсинг ББ-кодов *** //
+function parseBBCode( text ) {
+	text = text.replace(/\n/g, "<br>"); // переход на новую строку
+	text = text.replace(/\[b\](.*?)\[\/b\]/ig, "<b>$1</b>"); // жирный текст
+	text = text.replace(/\[i\](.*?)\[\/i\]/ig, "<i>$1</i>"); // наклонный текст
+	text = text.replace(/\[u\](.*?)\[\/u\]/ig, "<u>$1</u>"); // подчёркнутый текст
+	text = text.replace(/\[to\](.*?)\[\/to\]/ig, "<b>$1</b>"); // обращение
+	text = text.replace(/\[url\](.*?)\[\/url\]/ig, "<a href=\"$1\">$1</a>"); // ссылка
+	text = text.replace(/\[url=(.*?)\](.*?)\[\/url\]/ig, "<a href=\"$1\">$2</a>"); // ссылка
+	return text;
+}
+
+// *** Парсинг смайлов *** //
+function parseSmile( text ) {
+	text = text.replace(/:\)/g, '<img src="/uploads/smiles/01.gif" class="nodechat_slile">');
+	text = text.replace(/:D/g, '<img src="/uploads/smiles/02.gif" class="nodechat_slile">');
+	text = text.replace(/LOL/g, '<img src="/uploads/smiles/03.gif" class="nodechat_slile">');
+	text = text.replace(/:P/g, '<img src="/uploads/smiles/04.gif" class="nodechat_slile">');
+	text = text.replace(/:yes:/g, '<img src="/uploads/smiles/05.gif" class="nodechat_slile">');
+	text = text.replace(/:\(/g, '<img src="/uploads/smiles/06.gif" class="nodechat_slile">');
+	text = text.replace(/;\)/g, '<img src="/uploads/smiles/07.gif" class="nodechat_slile">');
+	text = text.replace(/:\'\(/g, '<img src="/uploads/smiles/08.gif" class="nodechat_slile">');
+	text = text.replace(/:!/g, '<img src="/uploads/smiles/09.gif" class="nodechat_slile">');
+	text = text.replace(/:no:/g, '<img src="/uploads/smiles/09.gif" class="nodechat_slile">');
+	return text;
+}
+
+// *** Формирование сообщения *** //
+exports.msg_format = function(msg, mess_id, name, user_group) {
+	// *** Парсим ББ-коды *** //
+	msg = parseBBCode(msg);
+
+	// *** Смайлы *** //
+	msg = parseSmile(msg);
+
+	// формирование строки времени
+	time = new Date(); // текущее время
+	time_line = addZero(time.getDate())+'.'+addZero(time.getMonth()+1)+' '+addZero(time.getHours())+':'+addZero(time.getMinutes())+':'+addZero(time.getSeconds());
+	// формирование строки сообщения
+	message = '<div id="nodechat_mess_'+mess_id+'" class="nodechat_mess">';
+	message += '<div class="nodechat_head">';
+	message += '<span id="nodechat_date_'+mess_id+'" class="nodechat_date" onclick="nodechat_action('+mess_id+')">[ '+time_line+' ]</span>';
+	message += '<span id="nodechat_edit_'+mess_id+'" style="display:none">';
+	message += '<a class="nodechat_pm" href="#" onclick="nodechat_PM(&quot;'+name+'&quot;); return !1;">[ ЛС ]</a>';
+	message += ' <a href="/user/'+encodeURI(name)+'">[ Профиль ]</a> ';
+	message += '<a class="nodechat_mess_del" href="#" onclick="nodechat_mess_del(&quot;'+mess_id+'&quot;); return !1;">[ Удалить ]</a>';
+	message += '</span>';
+	message += ' <b onclick="nodechat_reply(&quot;'+name+'&quot;); return !1;" class="nodechat_user fc_ugrp_'+user_group+'">'+name+'</b>';
+	message += '</div>';
+	message += '<div class="nodechat_text">'+msg+'</div>';
+	message += '</div>';
+	return message;
+}
+
+// *** Формирование строки всех сообщений *** //
+exports.mess_line = function(msg) {
+	var mess_tmp = msg.concat(); // создаём временным массив
+	mess_tmp.reverse(); // разворачиваем его в обратном порядке
+	return mess_tmp.join(''); // формируем строку со всеми сообщениями
+}
---
+++ server.js
@@ -0,0 +1,165 @@
+/*
+=====================================================
+ Модуль: NodeChat for DLE
+ Версия: 1.0.3
+-----------------------------------------------------
+ Автор: MSW
+ Сайт:  http://0-web.ru/
+-----------------------------------------------------
+ Copyright (c) 2012 MSW
+=====================================================
+ Данный код защищен авторскими правами
+=====================================================
+ Файл: server.js
+=====================================================
+*/
+var express = require('express'), // подключаем модуль express
+    crypto = require('crypto'), // подключаем модуль для шифрования
+    fs = require('fs'); // подключаем модуль для работы с файловой системой
+
+var func = require('./functions'), // подключаем файл с функциями
+    lang = require('./language'), // подключаем файл с языковыми переменными
+    config = require('./config'); // подключаем файл с настройками
+
+var app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(config.port);
+
+io.enable('browser client minification'); // сжатие *.js файлов
+io.enable('browser client etag');         // apply etag caching logic based on version number
+io.enable('browser client gzip');         // gzip the file
+io.set('log level', 0); // логировать только ошибки
+//io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile', 'flashsocket' ]); // enable all transports
+
+var mysql = require('mysql'); // подключаем модуль для работы с MySQL
+var db = mysql.createConnection({ //параметры подключения к базе
+	host     : config.mysql_host,		// хост MySQL
+	database : config.mysql_database,	// имя базы
+	user     : config.mysql_user,		// имя пользователя
+	password : config.mysql_password,	// пароль базы
+});
+
+var messages = [], // массив сообщений
+    member_id = [], // массив пользователей
+    mess_line = "", // строка со всеми сообщениями
+    mess_id = 0; // айди сообщения
+
+var html_chat = fs.readFileSync( __dirname + '/html/chat.html', 'utf-8');
+
+io.sockets.on('connection', function (socket) {
+	// ****** Идентификация пользователя ****** //
+	var cookies = func.parse_cookies(socket.handshake.headers.cookie);
+	fs.readFile( config.dir_phpsess+cookies.PHPSESSID, 'utf-8', function (err, data) {
+		var dle_user_id, dle_password, sessions;
+		// *** Идентификация по данным куков *** //
+		if(err) {
+			dle_user_id = cookies.dle_user_id;
+			dle_password = cookies.dle_password;
+		// *** Идентификация по данным сессии *** //
+		} else {
+			sessions = func.parse_session( data );
+			dle_user_id = sessions.dle_user_id;
+			dle_password = sessions.dle_password;
+		}
+		if( parseInt(dle_user_id)>0 && dle_password!=0 ) {
+			db.query('SELECT name, password, user_id, user_group, restricted FROM '+config.mysql_prefix+'_users WHERE user_id='+db.escape(dle_user_id), function(err, rows) {
+				if( rows[0].user_id && rows[0].password && rows[0].password == crypto.createHash('md5').update(dle_password).digest("hex") ) {
+					member_id[socket.id] = {name:rows[0].name, user_id:rows[0].user_id, user_group:rows[0].user_group, restricted:rows[0].restricted};
+				}
+				socket.emit('chat_join', "true");
+			});
+		} else {
+			member_id[socket.id] = {name:'guest', user_id:'0', user_group:'5', restricted:'1'};
+			socket.emit('chat_join', "true");
+		}
+	});
+
+	// *** Подкючение к комнате чата *** //
+	socket.on('join2chat', function() {
+		socket.join('chat');
+		var data = html_chat.replace('!MESSAGES!', mess_line);
+		if(member_id[socket.id]) {
+			if(member_id[socket.id].restricted>0) data = data.replace('!TEXTAREA!', lang.restricted);
+			else if(member_id[socket.id].restricted==0) data = data.replace('!TEXTAREA!', '<textarea id="nodechat_input"></textarea>');
+			else data = data.replace('!TEXTAREA!', lang.guest);
+		} else data = data.replace('!TEXTAREA!', lang.guest);
+		socket.emit('chat_init', data);
+	});
+
+	// *** Получение сообщение *** //
+	socket.on('chat_msg2server', function (msg) {
+		if(member_id[socket.id].restricted==0) {
+			msg = msg.replace(/^\s+/, "").replace(/\s+$/, ""); // удаляем проблемы в начале и конце текста
+			// минимальная длина сообщения
+			if(msg.length < config.mess_length_min) {
+				socket.emit( 'chat_msg2client', {info:'<div class="nodechat_info error">'+lang.msg_min+'</div>'} );
+			// максимальная длина сообщения
+			} else if(msg.length > config.mess_length_max) {
+				socket.emit( 'chat_msg2client', {info:'<div class="nodechat_info error">'+lang.msg_max+'</div>'} );
+			} else {
+				msg = msg.replace(/</g, "&lt;"); // фильтруем html
+				msg = msg.replace(/>/g, "&gt;"); // фильтруем html
+
+				// *** Отправка ЛС *** //
+				if(msg.indexOf('[PM]')+1 && msg.indexOf('[/PM]')+1) {
+					msg = msg.replace("[PM]", ""); //
+					var parts = msg.split('[/PM]:');
+					if(parts[0] && parts[1]) {
+						db.query('SELECT name, user_id FROM '+config.mysql_prefix+'_users WHERE name='+db.escape(parts[0]), function(err, rows) {
+							if( rows[0] ) {
+								var time = new Date(); // текущее время
+								db.query('INSERT INTO '+config.mysql_prefix+'_pm (subj, text, user, user_from, date, pm_read, folder) VALUES ("'+lang.ls_subj+'", '+db.escape(parts[1])+', "'+rows[0].user_id+'", "'+member_id[socket.id].name+'", "'+parseInt( time.getTime()/1000 )+'", "no", "inbox");');
+								db.query('UPDATE '+config.mysql_prefix+'_users SET pm_all=pm_all+1, pm_unread=pm_unread+1 WHERE user_id='+rows[0].user_id);
+								socket.emit( 'chat_msg2client', {info:'<div class="nodechat_info notis">'+lang.ls_send+'</div>'} );
+							} else socket.emit( 'chat_msg2client', {info:'<div class="nodechat_info error">'+lang.user_not_found+'</div>'} );
+						});
+					}
+				} else {
+					mess_id++; // айди сообщения
+					// *** формирование сообщения *** //
+					var message = func.msg_format(msg, mess_id, member_id[socket.id].name, member_id[socket.id].user_group);
+
+					if(mess_id>config.mess_limit) {
+						var mess_id_del = mess_id - config.mess_limit;
+						delete messages[mess_id_del];
+						io.sockets.in('chat').emit( 'chat_msg2client', {message:message, del:mess_id_del} );
+					} else {
+						io.sockets.in('chat').emit( 'chat_msg2client', {message:message} );
+					}
+					// дописываем сообщение в массив
+					messages[mess_id] = message;
+					// формируем строку всех сообщений
+					mess_line = func.mess_line(messages);
+				}
+			}
+		}
+	});
+
+	// *** Удаление сообщения *** //
+	socket.on('del_msg', function(id) {
+		if(member_id[socket.id]) {
+			if( member_id[socket.id].user_group==1 || member_id[socket.id].user_group==2 ) {
+				// удаляем сообщение
+				delete messages[id];
+				// отправляем всем команду на удаление сообщения
+				io.sockets.in('chat').emit( 'chat_msg2client', {del:id} );
+				// формируем строку всех сообщений
+				mess_line = func.mess_line(messages);
+			} else socket.emit( 'chat_msg2client', {info:'<div class="nodechat_info error">'+lang.access_denid+'</div>'} );
+		}
+	});
+
+	// *** Отключение пользователя *** //
+	socket.on('disconnect', function() {
+		delete member_id[socket.id];
+	});
+
+	// *** Отключение чата *** //
+	socket.on('leave_chat', function() {
+		socket.leave('chat');
+	});
+
+});
